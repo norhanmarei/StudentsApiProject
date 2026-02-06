5e000720e9216cf90be20b8b5214e6c88729ec65
Norhan Marei <mustafanour751@gmail.com>
2026-02-06 09:51:49 +0300

    refactor(business): turn Student into domain entity and add DTO mapping

--- StudentApiBusinessLayer/Student.cs
+++ StudentApiBusinessLayer/Student.cs
@@ -3,6 +3,24 @@ namespace StudentApiBusinessLayer;
 
 public class Student
 {
+    public enum enMode {AddNew=0, Update=1};
+    public enMode Mode = enMode.AddNew;
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public int Grade { get; set; }
+    public StudentDTO studentDto { get { return (new StudentDTO(this.Id, this.Name, this.Age, this.Grade)); } }
+    public Student(StudentDTO sDTO, enMode mode = enMode.AddNew)
+    {
+        this.Id = sDTO.Id;
+        this.Name = sDTO.Name;
+        this.Age = sDTO.Age;
+        this.Grade = sDTO.Grade;
+        Mode = mode;
+    }
+
+
+
     static public List<StudentDTO> GetAllStudents()
     {
         return StudentData.GetAllStudents();
@@ -15,9 +33,11 @@ public class Student
     {
         return Student.GetAverageGrade();
     }
-    static public StudentDTO? GetStudentById(int Id)
+    static public Student? GetStudentById(int Id)
     {
-        return StudentData.GetStudentById(Id);
+        StudentDTO studentDTO = StudentData.GetStudentById(Id);
+        if (studentDTO == null) return null;
+        return new Student(studentDTO, enMode.Update);
     }
 
 }
